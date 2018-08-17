3730af65f1ace3166fc1ecfb117c100194cbe56a
jairNeto <jguedesf@uci.edu>
2018-08-17 12:34:50 -0300

    Refatora funções de limpeza

--- processa_dados_votacao_candidato_munzona.R
+++ processa_dados_votacao_candidato_munzona.R
@@ -10,37 +10,48 @@ perfil_eleitorado_2016 <- readr::read_csv2(here::here("data/perfil_eleitorado_20
                                            local=readr::locale(encoding="latin1"),
                                            col_names = FALSE)
 
-col_names_candidatos <- tolower(c("DATA_GERACAO", "HORA_GERACAO","ANO_ELEICAO", "NUM_TURNO",
-               "DESCRICAO_ELEICAO", "SIGLA_UF", "SIGLA_UE", "CODIGO_MUNICIPIO",
-               "NOME_MUNICIPIO", "NUMERO_ZONA", "CODIGO_CARGO", "NUMERO_CAND",
-               "SQ_CANDIDATO", "NOME_CANDIDATO", "NOME_URNA_CANDIDATO", "DESCRICAO_CARGO",
-               "COD_SIT_CAND_SUPERIOR", "DESC_SIT_CAND_SUPERIOR", "CODIGO_SIT_CANDIDATO", "DESC_SIT_CANDIDATO",
-               "CODIGO_SIT_CAND_TOT", "DESC_SIT_CAND_TOT", "NUMERO_PARTIDO", "SIGLA_PARTIDO",
-               "NOME_PARTIDO", "SEQUENCIAL_LEGENDA", "NOME_COLIGACAO", "COMPOSICAO_LEGENDA",
-               "TOTAL_VOTOS", "TRANSITO"))
-col_names_partidos <- tolower(c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEICAO", "NUM_TURNO", "DESCRICAO_ELEICAO",
-                                "SIGLA_UF", "SIGLA_UE", "CODIGO_MUNICIPIO", "NOME_MUNICIPIO", "NUMERO_ZONA",
-                                "CODIGO_CARGO", "DESCRICAO_CARGO", "TIPO_LEGENDA", "NOME_COLIGACAO",
-                                "COMPOSICAO_LEGENDA", "SIGLA_PARTIDO", "NUMERO_PARTIDO", "NOME_PARTIDO",
-                                "QTDE_VOTOS_NOMINAIS", "QTDE_VOTOS_LEGENDA", "TRANSITO", "SEQUENCIAL_COLIGACAO"))
-col_names_eleitorado <- tolower(c("PERIODO", "UF", "MUNICIPIO", "COD_MUNICIPIO_TSE", "NR_ZONA", "SEXO",
-                                  "FAIXA_ETARIA", "GRAU_DE_ESCOLARIDADE", "QTD_ELEITORES_NO_PERFIL"))
+formata_candidatos_munzona <- function(ano = 2016, data) {
+  col_names_candidatos <- tolower(c("DATA_GERACAO", "HORA_GERACAO","ANO_ELEICAO", "NUM_TURNO",
+                                    "DESCRICAO_ELEICAO", "SIGLA_UF", "SIGLA_UE", "CODIGO_MUNICIPIO",
+                                    "NOME_MUNICIPIO", "NUMERO_ZONA", "CODIGO_CARGO", "NUMERO_CAND",
+                                    "SQ_CANDIDATO", "NOME_CANDIDATO", "NOME_URNA_CANDIDATO", "DESCRICAO_CARGO",
+                                    "COD_SIT_CAND_SUPERIOR", "DESC_SIT_CAND_SUPERIOR", "CODIGO_SIT_CANDIDATO", "DESC_SIT_CANDIDATO",
+                                    "CODIGO_SIT_CAND_TOT", "DESC_SIT_CAND_TOT", "NUMERO_PARTIDO", "SIGLA_PARTIDO",
+                                    "NOME_PARTIDO", "SEQUENCIAL_LEGENDA", "NOME_COLIGACAO", "COMPOSICAO_LEGENDA",
+                                    "TOTAL_VOTOS", "TRANSITO"))
+  
+  names(data) <- col_names_candidatos
+  
+  data %>%
+    dplyr::select(ano_eleicao, num_turno, sigla_uf, nome_municipio, nome_candidato, nome_urna_candidato, descricao_cargo, numero_cand,
+                  desc_sit_cand_superior, desc_sit_candidato, desc_sit_cand_tot, sigla_partido, nome_coligacao, composicao_legenda,
+                  total_votos,transito)
+}
+formata_partidos_munzona <- function(ano = 2016, data) {
+  col_names_partidos <- tolower(c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEICAO", "NUM_TURNO", "DESCRICAO_ELEICAO",
+                                  "SIGLA_UF", "SIGLA_UE", "CODIGO_MUNICIPIO", "NOME_MUNICIPIO", "NUMERO_ZONA",
+                                  "CODIGO_CARGO", "DESCRICAO_CARGO", "TIPO_LEGENDA", "NOME_COLIGACAO",
+                                  "COMPOSICAO_LEGENDA", "SIGLA_PARTIDO", "NUMERO_PARTIDO", "NOME_PARTIDO",
+                                  "QTDE_VOTOS_NOMINAIS", "QTDE_VOTOS_LEGENDA", "TRANSITO", "SEQUENCIAL_COLIGACAO"))
+  
+  names(data) <- col_names_partidos
+  
+  data %>%
+    dplyr::select(ano_eleicao, num_turno, sigla_uf, nome_municipio, descricao_cargo, nome_coligacao, composicao_legenda, sigla_partido,
+                  numero_partido, qtde_votos_nominais, qtde_votos_legenda, transito)
+}
+formata_eleitorado <- function(ano = 2016, data) {
+  col_names_eleitorado <- tolower(c("PERIODO", "UF", "MUNICIPIO", "COD_MUNICIPIO_TSE", "NR_ZONA", "SEXO",
+                                    "FAIXA_ETARIA", "GRAU_DE_ESCOLARIDADE", "QTD_ELEITORES_NO_PERFIL"))
+  
+  names(data) <- col_names_eleitorado
+  
+  data %>%
+    dplyr::select(-c(cod_municipio_tse, nr_zona))
+}
 
-names(munzona_candidatos_data) <- col_names_candidatos
-names(munzona_partidos_data) <- col_names_partidos
-names(perfil_eleitorado_2016) <- col_names_eleitorado
-
-munzona_candidatos_data <- 
-  munzona_candidatos_data %>%
-  dplyr::select(ano_eleicao, num_turno, sigla_uf, nome_municipio, nome_candidato, nome_urna_candidato, descricao_cargo, numero_cand,
-                desc_sit_cand_superior, desc_sit_candidato, desc_sit_cand_tot, sigla_partido, nome_coligacao, composicao_legenda,
-                total_votos,transito)
-munzona_partidos_data <-
-  munzona_partidos_data %>%
-  dplyr::select(ano_eleicao, num_turno, sigla_uf, nome_municipio, descricao_cargo, nome_coligacao, composicao_legenda, sigla_partido,
-                numero_partido, qtde_votos_nominais, qtde_votos_legenda, transito)
-perfil_eleitorado_2016 <-
-  perfil_eleitorado_2016 %>%
-  dplyr::select(-c(cod_municipio_tse, nr_zona))
+munzona_candidatos_data <- formata_candidatos_munzona(2016, munzona_candidatos_data)
+munzona_partidos_data <- formata_partidos_munzona(2016, munzona_partidos_data)
+perfil_eleitorado_2016 <-formata_eleitorado(2016, perfil_eleitorado_2016)
 
 
