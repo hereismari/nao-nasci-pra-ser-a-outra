cee94502b76b865f09f7415297171f5268aa9e78
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2018-08-17 16:33:51 -0300

    Adicionado script que extrai informações sobre candidatos

--- scripts/processa_dados_votacao_candidato_munzona.R
+++ scripts/processa_dados_votacao_candidato_munzona.R
@@ -3,9 +3,11 @@ library(tidyverse)
 munzona_candidatos_data <- readr::read_csv2(here::here("data/votacao_candidato_munzona_2016/votacao_candidato_munzona_2016_PB.csv"),
                                  local=readr::locale(encoding="latin1"),
                                  col_names = FALSE)
+
 munzona_partidos_data <- readr::read_csv2(here::here("data/votacao_partido_munzona_2016/votacao_partido_munzona_2016_PB.csv"),
                                             local=readr::locale(encoding="latin1"),
                                             col_names = FALSE)
+
 perfil_eleitorado_2016 <- readr::read_csv2(here::here("data/perfil_eleitorado_2016/perfil_eleitorado_2016.csv"),
                                            local=readr::locale(encoding="latin1"),
                                            col_names = FALSE)
@@ -33,7 +35,7 @@ formata_candidatos_munzona <- function(ano = 2016, data) {
   data %>%
     dplyr::group_by(sq_candidato, ano_eleicao, num_turno, sigla_uf, nome_municipio, nome_candidato, nome_urna_candidato, descricao_cargo, numero_cand,
                     desc_sit_cand_superior, desc_sit_candidato, desc_sit_cand_tot, sigla_partido, nome_coligacao, composicao_legenda,
-                    transito) %>%
+                    transito, numero_cand) %>%
     summarise(total_votos = sum(total_votos)) 
 }
 formata_partidos_munzona <- function(ano = 2016, data) {
@@ -63,5 +65,34 @@ all_munzona_candidatos_data <- formata_candidatos_munzona(2016, all_munzona_cand
 all_munzona_partidos_data <- formata_partidos_munzona(2016, all_munzona_partidos_data)
 perfil_eleitorado_2016 <-formata_eleitorado(2016, perfil_eleitorado_2016)
 
-write.csv2(all_munzona_candidatos_data, "all_munzona_candidatos_2016.csv")
 
+# Importa dados
+candidatos_data <- readr::read_csv2("data/consulta_cand_2016/merged.csv",
+                                    local=locale(encoding = "latin1"))
+
+names(candidatos_data) = c("data_geracao", "hora_geracao", "ano_eleicao",
+                           "num_turno", "descricao_eleicao", "sigla_uf",
+                           "cod_cidade", "nome_municipio", "cod_cargo", "descricao_cargo", 
+                           "nome_candidato",
+                           "sequencial_candidato", "numero_cand","cpf_candidato",
+                           "nome_urna_candidato","cod_situacao_candidatura", 
+                           "desc_sit_cand_superior", "numero_partido", "sigla_partido",
+                           "nome_partido", "codigo_legenda","sigla_legenda", 
+                           "composicao_legenda", "nome_coligacao", "codigo_ocupacao",
+                           "descricao_ocupacao", "data_nascimento", 
+                           "num_tit_eleitoral_candidato",
+                           "idade_data_candidato", "cod_sexo", "sexo", 
+                           "cod_grau_instrucao",
+                           "grau_instrucao", "cod_estado_civil", "estado_civil",
+                           "cod_cor_raca", "cor_raca", "codigo_nacionalidade",
+                           "descricao_nacionalidade", "sigla_UF_nascimento",
+                           "cod_municipio_nascimento", "nome_municipio_nascimento",
+                           "despesa_max_campanha", "cod_sit_tot_turno",
+                           "desc_sit_cand_tot", "email")
+
+sexo_raca_df <- candidatos_data %>% dplyr::select(numero_cand, sexo, cor_raca, nome_municipio, sigla_uf, descricao_cargo)
+
+data <- left_join(all_munzona_candidatos_data, sexo_raca_df, by=c("numero_cand", "nome_municipio", "descricao_cargo", "sigla_uf"))
+
+write.csv2(data, "data/candidatos_2016.csv")
+write.csv2(all_munzona_candidatos_data, "data/all_munzona_candidatos_2016.csv")
