94faac571427086a26b0ebdabba625a37c67b1cc
Marianne Linhares Monteiro <mariannelinharesm@gmail.com>
2018-08-18 08:59:14 -0300

    Refatorando rotas e nomes de funções

--- server/router.py
+++ server/router.py
@@ -17,35 +17,35 @@ client = Client(app)
 
 # Rotas
 @app.route('/partidos/participacao/mulheres', methods=['GET'])
-def participacao_mulheres():
+def partidos_participacao_mulheres():
     _args = utils.mount_request(args.CANDIDATOS)
-    return utils.jsonify(client.participacao_mulheres(_args))
+    return utils.jsonify(client.partidos_participacao_mulheres(_args))
 
 
 @app.route('/partidos/ranking/zerovotos', methods=['GET'])
-def ranking_zero_votos():
-    return utils.jsonify(client.ranking_zero_votos())
+def partidos_ranking_zero_votos():
+    _args = utils.mount_request(args.CANDIDATOS)
+    return utils.jsonify(client.partidos_ranking_poucos_votos(_args, 0))
 
 
 @app.route('/partidos/ranking/poucosvotos', methods=['GET'])
-def ranking_poucos_votos():
+def partidos_ranking_poucos_votos():
     _args = utils.mount_request(args.CANDIDATOS)
     poucos_votos = request.args.get('poucos_votos', default=3, type=int)
-    return utils.jsonify(client.ranking_poucos_votos(_args, poucos_votos))
-
+    return utils.jsonify(client.partidos_ranking_poucos_votos(_args, poucos_votos))
 
 
 @app.route('/candidatos/zerovotos', methods=['GET'])
-def zero_votos():
+def candidatos_zero_votos():
     _args = utils.mount_request(args.CANDIDATOS)
-    return utils.jsonify(client.zero_votos(_args))
+    return utils.jsonify(client.candidatos_poucos_votos(_args, 0))
 
 
 @app.route('/candidatos/poucosvotos', methods=['GET'])
-def poucos_votos():
+def candidatos_poucos_votos():
     _args = utils.mount_request(args.CANDIDATOS)
     poucos_votos = request.args.get('poucos_votos', default=3, type=int)
-    return utils.jsonify(client.poucos_votos(_args, poucos_votos))
+    return utils.jsonify(client.candidatos_poucos_votos(_args, poucos_votos))
 
 
 # Main
