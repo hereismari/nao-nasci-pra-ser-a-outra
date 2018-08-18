cd118c4460707673c8070e6fbcd9e1046cc8c160
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2018-08-17 21:22:42 -0300

    Adicionada função que processa o gênero de todos os anos

--- r_scripts/processa_candidato.R
+++ r_scripts/processa_candidato.R
@@ -66,33 +66,77 @@ all_munzona_partidos_data <- formata_partidos_munzona(2016, all_munzona_partidos
 perfil_eleitorado_2016 <-formata_eleitorado(2016, perfil_eleitorado_2016)
 
 
-# Importa dados
-candidatos_data <- readr::read_csv2("data/consulta_cand_2016/merged.csv",
-                                    local=locale(encoding = "latin1"))
 
-names(candidatos_data) = c("data_geracao", "hora_geracao", "ano_eleicao",
-                           "num_turno", "descricao_eleicao", "sigla_uf",
-                           "cod_cidade", "nome_municipio", "cod_cargo", "descricao_cargo", 
-                           "nome_candidato",
-                           "sequencial_candidato", "numero_cand","cpf_candidato",
-                           "nome_urna_candidato","cod_situacao_candidatura", 
-                           "desc_sit_cand_superior", "numero_partido", "sigla_partido",
-                           "nome_partido", "codigo_legenda","sigla_legenda", 
-                           "composicao_legenda", "nome_coligacao", "codigo_ocupacao",
-                           "descricao_ocupacao", "data_nascimento", 
-                           "num_tit_eleitoral_candidato",
-                           "idade_data_candidato", "cod_sexo", "sexo", 
-                           "cod_grau_instrucao",
-                           "grau_instrucao", "cod_estado_civil", "estado_civil",
-                           "cod_cor_raca", "cor_raca", "codigo_nacionalidade",
-                           "descricao_nacionalidade", "sigla_UF_nascimento",
-                           "cod_municipio_nascimento", "nome_municipio_nascimento",
-                           "despesa_max_campanha", "cod_sit_tot_turno",
-                           "desc_sit_cand_tot", "email")
 
-sexo_raca_df <- candidatos_data %>% dplyr::select(numero_cand, sexo, cor_raca, nome_municipio, sigla_uf, descricao_cargo)
 
 data <- left_join(all_munzona_candidatos_data, sexo_raca_df, by=c("numero_cand", "nome_municipio", "descricao_cargo", "sigla_uf"))
 
-write.csv2(data, "data/candidatos_2016.csv")
-write.csv2(all_munzona_candidatos_data, "data/all_munzona_candidatos_2016.csv")
+write.csv2(data, "data/preprocessed/candidatos_2016.csv")
+write.csv2(all_munzona_candidatos_data, "data/preprocessed/all_munzona_candidatos_2016.csv")
+
+
+fetch_gender_info <- function(ano = 2016) {
+  data_path <- here::here(paste0("data/Candidatos/consulta_cand_", ano, "/merged.csv"))
+  candidatos_data <- readr::read_csv2(data_path, local=locale(encoding = "latin1")) 
+  if(ano <= 2010) {
+    col_names <- col_names <- c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEICAO", "NUM_TURNO",
+                                "DESCRICAO_ELEICAO", "SIGLA_UF",
+                                "SIGLA_UE", "NOME_MUNICIPIO", "CODIGO_CARGO", "DESCRICAO_CARGO",
+                                "NOME_CANDIDATO", "SEQUENCIAL_CANDIDATO", "NUMERO_CAND",
+                                "CPF_CANDIDATO", "NOME_URNA_CANDIDATO", "COD_SITUACAO_CANDIDATURA",
+                                "DES_SITUACAO_CANDIDATURA", "NUMERO_PARTIDO", "SIGLA_PARTIDO",
+                                "NOME_PARTIDO", "CODIGO_LEGENDA", "SIGLA_LEGENDA", "COMPOSICAO_LEGENDA",
+                                "NOME_LEGENDA", "CODIGO_OCUPACAO", "DESCRICAO_OCUPACAO",
+                                "DATA_NASCIMENTO","NUM_TITULO_ELEITORAL_CANDIDATO", "IDADE_DATA_ELEICAO",
+                                "CODIGO_SEXO", "SEXO", "COD_GRAU_INSTRUCAO",
+                                "DESCRICAO_GRAU_INSTRUCAO", "CODIGO_ESTADO_CIVIL",
+                                "DESCRICAO_ESTADO_CIVIL", "CODIGO_NACIONALIDADE",
+                                "DESCRICAO_NACIONALIDADE", "SIGLA_UF_NASCIMENTO",
+                                "CODIGO_MUNICIPIO_NASCIMENTO", "NOME_MUNICIPIO_NASCIMENTO",
+                                "DESPESA_MAX_CAMPANHA","COD_SIT_TOT_TURNO",
+                                "DESC_SIT_TOT_TURNO") %>% tolower()
+  } else if (ano == 2012){
+    col_names <- c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEICAO", "NUM_TURNO",
+                   "DESCRICAO_ELEICAO", "SIGLA_UF",
+                   "SIGLA_UE", "NOME_MUNICIPIO", "CODIGO_CARGO", "DESCRICAO_CARGO",
+                   "NOME_CANDIDATO", "SEQUENCIAL_CANDIDATO", "NUMERO_CAND",
+                   "CPF_CANDIDATO", "NOME_URNA_CANDIDATO", "COD_SITUACAO_CANDIDATURA",
+                   "DES_SITUACAO_CANDIDATURA", "NUMERO_PARTIDO", "SIGLA_PARTIDO",
+                   "NOME_PARTIDO", "CODIGO_LEGENDA", "SIGLA_LEGENDA", "COMPOSICAO_LEGENDA",
+                   "NOME_LEGENDA", "CODIGO_OCUPACAO", "DESCRICAO_OCUPACAO",
+                   "DATA_NASCIMENTO","NUM_TITULO_ELEITORAL_CANDIDATO", "IDADE_DATA_ELEICAO",
+                   "CODIGO_SEXO", "SEXO", "COD_GRAU_INSTRUCAO",
+                   "DESCRICAO_GRAU_INSTRUCAO", "CODIGO_ESTADO_CIVIL",
+                   "DESCRICAO_ESTADO_CIVIL", "CODIGO_NACIONALIDADE",
+                   "DESCRICAO_NACIONALIDADE", "SIGLA_UF_NASCIMENTO",
+                   "CODIGO_MUNICIPIO_NASCIMENTO", "NOME_MUNICIPIO_NASCIMENTO",
+                   "DESPESA_MAX_CAMPANHA","COD_SIT_TOT_TURNO",
+                   "DESC_SIT_TOT_TURNO", "NM_EMAIL") %>% tolower()
+
+  } else if (ano >= 2014) {
+    col_names <- c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEICAO", "NUM_TURNO",
+                   "DESCRICAO_ELEICAO", "SIGLA_UF",
+                   "SIGLA_UE", "NOME_MUNICIPIO", "CODIGO_CARGO", "DESCRICAO_CARGO",
+                   "NOME_CANDIDATO", "SEQUENCIAL_CANDIDATO", "NUMERO_CAND",
+                   "CPF_CANDIDATO", "NOME_URNA_CANDIDATO", "COD_SITUACAO_CANDIDATURA",
+                   "DES_SITUACAO_CANDIDATURA", "NUMERO_PARTIDO", "SIGLA_PARTIDO",
+                   "NOME_PARTIDO", "CODIGO_LEGENDA", "SIGLA_LEGENDA", "COMPOSICAO_LEGENDA",
+                   "NOME_LEGENDA", "CODIGO_OCUPACAO", "DESCRICAO_OCUPACAO",
+                   "DATA_NASCIMENTO","NUM_TITULO_ELEITORAL_CANDIDATO", "IDADE_DATA_ELEICAO",
+                   "CODIGO_SEXO", "SEXO", "COD_GRAU_INSTRUCAO",
+                   "DESCRICAO_GRAU_INSTRUCAO", "CODIGO_ESTADO_CIVIL",
+                   "DESCRICAO_ESTADO_CIVIL", "CODIGO_COR_RACA",
+                   "COR_RACA", "CODIGO_NACIONALIDADE",
+                   "DESCRICAO_NACIONALIDADE", "SIGLA_UF_NASCIMENTO",
+                   "CODIGO_MUNICIPIO_NASCIMENTO", "NOME_MUNICIPIO_NASCIMENTO",
+                   "DESPESA_MAX_CAMPANHA","COD_SIT_TOT_TURNO",
+                   "DESC_SIT_TOT_TURNO", "NM_EMAIL") %>% tolower()
+  }
+  names(candidatos_data) <- col_names
+  candidatos_data %>% dplyr::select(numero_cand, sexo, nome_municipio, sigla_uf, descricao_cargo)
+}
+
+process_candidato <- function(ano=2016) {
+  genero_data <- fetch_gender_info(ano)
+}
+
