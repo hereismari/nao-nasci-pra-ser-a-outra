adb25a90a747cb4f084b94311936fdfea7557d30
Dandara Sousa <dandara.sousa@ccc.ufcg.edu.br>
2018-08-17 21:36:36 -0300

    falta arrumar o tooltip

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,16 @@ import "./App.css";
 import Podio from "./components/visualizacoes/Podio";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import Proporcao from "./components/visualizacoes/Proporcao";
+import VotosVSInvestimentos from "./components/visualizacoes/VotosVSInvestimentos";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Podio />
+        <VotosVSInvestimentos />
         <Footer />
       </div>
     );
---
+++ client/src/components/visualizacoes/Ranking.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import NVD3Chart from "react-nvd3/dist/react-nvd3";
+
+export default class Ranking extends Component{
+    
+}
