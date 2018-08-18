c6af031b04c82e1af9f22a3e578e101b6d63a9e1
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2018-08-18 11:42:38 -0300

    Ajustado process_eleitores e retirando colunas indesejadas do script em process_candidatos

--- r_scripts/processa_candidato.R
+++ r_scripts/processa_candidato.R
@@ -24,8 +24,8 @@ formata_candidatos_munzona <- function(ano = 2016, data) {
   names(data) <- col_names_candidatos
   
   data %>%
-    dplyr::group_by(sq_candidato, ano_eleicao, num_turno, sigla_uf, nome_municipio, nome_candidato, nome_urna_candidato, descricao_cargo, numero_cand,
-                    desc_sit_cand_superior, desc_sit_candidato, desc_sit_cand_tot, sigla_partido, nome_coligacao, composicao_legenda,
+    dplyr::group_by(ano_eleicao, sigla_uf, nome_municipio, nome_urna_candidato, descricao_cargo, numero_cand,
+                    desc_sit_candidato, desc_sit_cand_tot, sigla_partido, nome_coligacao, composicao_legenda,
                     numero_cand) %>%
     summarise(total_votos = sum(total_votos)) 
 }
@@ -39,8 +39,8 @@ formata_partidos_munzona <- function(ano = 2016, data) {
   names(data) <- col_names_partidos
   
   data %>%
-    dplyr::select(ano_eleicao, num_turno, sigla_uf, nome_municipio, descricao_cargo, nome_coligacao, composicao_legenda, sigla_partido,
-                  numero_partido, qtde_votos_nominais, qtde_votos_legenda, transito)
+    dplyr::select(ano_eleicao, sigla_uf, nome_municipio, descricao_cargo, nome_coligacao, composicao_legenda, sigla_partido,
+                  numero_partido, qtde_votos_nominais, qtde_votos_legenda)
 }
 formata_eleitorado <- function(ano = 2016, data) {
   col_names_eleitorado <- tolower(c("PERIODO", "UF", "MUNICIPIO", "COD_MUNICIPIO_TSE", "NR_ZONA", "SEXO",
@@ -54,7 +54,8 @@ formata_eleitorado <- function(ano = 2016, data) {
 
 fetch_gender_info <- function(ano = 2016) {
   data_path <- here::here(paste0("data/Candidatos/consulta_cand_", ano, "/merged.csv"))
-  candidatos_data <- readr::read_csv2(data_path, local=locale(encoding = "latin1")) 
+  candidatos_data <- readr::read_csv2(data_path, local=readr::locale(enconding=)) 
+  
   if(ano <= 2010) {
     col_names <- col_names <- c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEICAO", "NUM_TURNO",
                                 "DESCRICAO_ELEICAO", "SIGLA_UF",
@@ -120,7 +121,7 @@ export_candidato <- function(ano=2016) {
 
 processa_candidatos <- function(ano=2016) {
   all_munzona_candidatos_data <- readr::read_csv2(here::here(paste0("data/Votacoes/votacao_candidato_munzona_", ano, "/merged.csv")),
-                                                  local=readr::locale(encoding="latin1"),
+                                                  local=readr::locale("br"),
                                                   col_names = FALSE)
   
   all_munzona_candidatos_data <- formata_candidatos_munzona(ano, all_munzona_candidatos_data)
@@ -157,11 +158,13 @@ data_2016 <- readr::read_csv2(here::here("data/preprocessed/candidatos/candidato
 data <- rbind(data_2014, data_2016)
                                              
 write.csv2(data, "data/preprocessed/all_candidatos_2014_e_2016.csv")
-data <- readr::read_csv2(here::here("data/preprocessed/all_candidatos_2014_e_2016.csv"),
-                              local=readr::locale(encoding="latin1"))
+
+data <- readr::read_csv2(here::here("data/preprocessed/all_candidatos_2010_a_2016.csv"),
+                              local=readr::locale("br"))
 
 mulheres <-  data %>% filter(sexo == "FEMININO") 
 
+data <- data %>% select(-nome_candidato)
 
 mulheres_zero <- mulheres %>% filter(total_votos == 0) %>% group_by(ano_eleicao) %>% summarise(cont_votos_zero = n())
 
--- r_scripts/processa_eleitores.R
+++ r_scripts/processa_eleitores.R
@@ -1,16 +1,21 @@
 library(tidyverse)
 
+perfil_eleitorado_2010 = readr::read_csv2(here::here("data/Eleitores/perfil_eleitorado_2010/perfil_eleitorado_2010.csv"), local=readr::locale(encoding="latin1"), col_names = FALSE)
 perfil_eleitorado_2012 = readr::read_csv2(here::here("data/Eleitores/perfil_eleitorado_2012/perfil_eleitorado_2012.csv"), local=readr::locale(encoding="latin1"), col_names = FALSE)
 perfil_eleitorado_2014 = readr::read_csv2(here::here("data/Eleitores/perfil_eleitorado_2014/perfil_eleitorado_2014.csv"), local=readr::locale(encoding="latin1"), col_names = FALSE)
 perfil_eleitorado_2016 = readr::read_csv2(here::here("data/Eleitores/perfil_eleitorado_2016/perfil_eleitorado_2016.csv"), local=readr::locale(encoding="latin1"), col_names = FALSE)
-perfil_eleitorado_ATUAL = readr::read_csv2(here::here("data/Eleitores/perfil_eleitorado_ATUAL/perfil_eleitorado_ATUAL.csv"), local=readr::locale(encoding="latin1"), col_names = FALSE)
+#perfil_eleitorado_ATUAL = readr::read_csv2(here::here("data/Eleitores/perfil_eleitorado_ATUAL/perfil_eleitorado_ATUAL.csv"), local=readr::locale(encoding="latin1"), col_names = FALSE)
 
-eleitores_all = rbind(perfil_eleitorado_2012, perfil_eleitorado_2014)
+eleitores_all = rbind(perfil_eleitorado_2010, perfil_eleitorado_2012)
+eleitores_all = rbind(eleitores_all, perfil_eleitorado_2014)
 eleitores_all = rbind(eleitores_all, perfil_eleitorado_2016)
-eleitores_all = rbind(eleitores_all, perfil_eleitorado_ATUAL)
+#eleitores_all = rbind(eleitores_all, perfil_eleitorado_ATUAL)
 
-colnames(eleitores_all) = c("nome", "uf", "cidade", "x4", "x5", "genero", "faixa_etaria", "escolaridade", "idade")
+colnames(eleitores_all) = c("ano", "sigla-uf", "cidade", "x4", "x5", "genero", "faixa_etaria", "escolaridade", "qtd_eleitores_no_perfil")
 eleitores_all = eleitores_all %>%
   select(-c("x4", "x5"))
 
-write.csv2(eleitores_all, "data/preprocessed/eleitores_all.csv")
+
+eleitores_all$ano <- sub("^(\\d{4}).*$", "\\1", eleitores_all$ano)
+eleitores_all <- eleitores_all %>% select(-qtd_eleitores_no_perfil) %>% distinct()
+write.csv2(eleitores_all, "data/preprocessed/all_eleitores.csv", row.names=FALSE)
