e561b4302c5427c8c18ec8be04e29313af8eaa55
Bob Farias <bobhenrique9@gmail.com>
2018-08-19 04:03:03 -0300

    so falta o footer

--- client/src/App.js
+++ client/src/App.js
@@ -24,9 +24,10 @@ class App extends Component {
       <div className="App">
         <div className="container-fluid">
           <div className="row part1">
+            <div className="bg col-md-12 col-xs-12 col-12 col-sm-12"></div>
                 <div className="col-md-6 col-xs-6 col-12 col-sm-12">
                   <div className="left-container">
-                    <span>30%</span><h3>Lorem Ipsum dollor sit amet</h3>
+                    <span className="blink_me">30%</span><h3>Lorem Ipsum dollor sit amet</h3>
                     <img src={require("./img/img-left-part1.png")} />
                   </div>
                 </div>
@@ -43,7 +44,7 @@ class App extends Component {
               <div className="row">
                   <div className="col-md-6 col-xs-6 col-12 col-sm-12 text1-part2">
                     <div>
-                      <span>30%</span><h3>Lorem Ipsum dollor sit amet</h3>
+                      <span className="blink_me">30%</span><h3>Lorem Ipsum dollor sit amet</h3>
                     </div>
                   </div>
                   <div className="col-md-6 col-xs-6 col-12 col-sm-12 text2-part2">
@@ -60,7 +61,7 @@ class App extends Component {
             <div className="col-md-4 col-xs-4 col-12 col-sm-12 text-part3">
                 <div>
                   <div className="row">
-                      <span>30%</span>
+                      <span className="blink_me">30%</span>
                       <div className="ajuste-porcent-sub">
                         <h3>Lorem Ipsum dollor sit amet</h3>
                       </div>
@@ -72,81 +73,118 @@ class App extends Component {
           <div className="row part4">
             <h1 className="col-md-12 col-xs-12 col-12 col-sm-12">Lorem Ipsum dollor sit amet</h1>
             <div className="col-md-12 col-xs-12 col-12 col-sm-12">
-              <div className="row">
+              <div className="row row-ranking">
                 <div className="col-md-4 col-xs-4 col-12 col-sm-12">
-                  <div className="card">
                     <div className="row rr-1">
                       <div className="rr1-img-part">
-                        <img src={require("./img/r1.jpg")} />
+                        <img src={require("./img/s1.svg")} />
                       </div>
                       <div className="rr1-partido">
                         <p className="nomepartido">PSDB</p>
+                        <span className="ttpartido"><i class="fab fa-twitter"></i>PSDB</span>
                       </div>
                     </div>
-                    <div className="row rr-2">
-                      <div className="ghost-size">
-                        <img src={require("./img/pac-man-ghost.svg")} />
+                    <div className="card">
+                      <div className="row rr-2">
+                        <div className="ghost-size">
+                          <img src={require("./img/pac-man-ghost.svg")} />
+                        </div>
+                        <div className="nota"><h3>321 candidatas fantasmas</h3></div>
                       </div>
-                      <div className="nota"><h3>321 fantasmas</h3></div>
-                    </div>
-                    <div className="btn">
-                      <div className="denuncia">
-                        <p>Exiga explicações!</p>
+                      <div className="botao">
+                        <div className="denuncia">
+                          <p>Exija explicações!<i class="fab fa-twitter"></i></p>
+                        </div>
                       </div>
-                    </div>
                   </div>
                 </div>
 
                  <div className="col-md-4 col-xs-4 col-12 col-sm-12">
-                  <div className="card">
                     <div className="row rr-1">
                       <div className="rr1-img-part">
-                        <img src={require("./img/r1.jpg")} />
+                        <img src={require("./img/s2.svg")} />
                       </div>
                       <div className="rr1-partido">
                         <p className="nomepartido">PSDB</p>
+                        <span className="ttpartido"><i class="fab fa-twitter"></i>PSDB</span>
                       </div>
                     </div>
-                    <div className="row rr-2">
-                      <div className="ghost-size">
-                        <img src={require("./img/pac-man-ghost.svg")} />
+                    <div className="card">
+                      <div className="row rr-2">
+                        <div className="ghost-size">
+                          <img src={require("./img/pac-man-ghost.svg")} />
+                        </div>
+                        <div className="nota"><h3>321 candidatas fantasmas</h3></div>
                       </div>
-                      <div className="nota"><h3>321 fantasmas</h3></div>
-                    </div>
-                    <div className="btn">
-                      <div className="denuncia">
-                        <p>Exiga explicações!</p>
+                      <div className="botao">
+                        <div className="denuncia">
+                          <p>Exija explicações!<i class="fab fa-twitter"></i></p>
+                        </div>
                       </div>
-                    </div>
                   </div>
                 </div>
 
             <div className="col-md-4 col-xs-4 col-12 col-sm-12">
-                  <div className="card">
                     <div className="row rr-1">
                       <div className="rr1-img-part">
-                        <img src={require("./img/r1.jpg")} />
+                        <img src={require("./img/s3.svg")} />
                       </div>
                       <div className="rr1-partido">
                         <p className="nomepartido">PSDB</p>
+                        <span className="ttpartido"><i class="fab fa-twitter"></i>PSDB</span>
                       </div>
                     </div>
-                    <div className="row rr-2">
-                      <div className="ghost-size">
-                        <img src={require("./img/pac-man-ghost.svg")} />
+                    <div className="card">
+                      <div className="row rr-2">
+                        <div className="ghost-size">
+                          <img src={require("./img/pac-man-ghost.svg")} />
+                        </div>
+                        <div className="nota"><h3>321 candidatas fantasmas</h3></div>
                       </div>
-                      <div className="nota"><h3>321 fantasmas</h3></div>
-                    </div>
-                    <div className="btn">
-                      <div className="denuncia">
-                        <p>Exiga explicações!</p>
+                      <div className="botao">
+                        <div className="denuncia">
+                          <p>Exija explicações!<i class="fab fa-twitter"></i></p>
+                        </div>
                       </div>
-                    </div>
                   </div>
                 </div>
-
               </div>
             </div>
+          </div>
+          <div className="row part5">
+            <h1 className="col-md-12 col-xs-12 col-12 col-sm-12">Lorem Ipsum dollor sit amet</h1>
+            <div className="col-md-6 col-xs-6 col-12 col-sm-12">
+                <div className="left-container">
+                   <span className="blink_me">30%</span><h3>Lorem Ipsum dollor sit amet</h3>
+                    <img src={require("./img/dilmae.gif")} />
+                </div>
+            </div>
+            <div className="col-md-6 col-xs-6 col-12 col-sm-12 ajust-margin">
+                <div className="colaborartext">
+                  <div className="row">
+                    <div className="megaphonediv"><img src={require("./img/megaphone.svg")} /></div>
+                    <p className="ajust-margin2 blink_me">Como colaborar?</p>
+                  </div>
+                    <h3>Pesquisa quantitativa não probabilística. Formulário respondido no Google Docs e compartilhado organicamente entre mulheres que trabalham em agência de publicidade</h3>
+                </div>
+            </div>
+          </div>
+          <div className="row part6">
+            <img className="ajust-img-pocs" src={require("./img/bob.jpg")} />
+              <span className="text-footer">Bob Henrique Farias</span>
+            <img className="ajust-img-pocs" src={require("./img/dan.jpg")} />
+              <span className="text-footer">Dandara Maria da Costa</span>
+            <img className="ajust-img-pocs" src={require("./img/hadri.jpg")} />
+              <span className="text-footer">Hadrizia Santos</span>
+            <img className="ajust-img-pocs" src={require("./img/italo.jpg")} />
+              <span className="text-footer">Italo Medeiros</span>
+            <img className="ajust-img-pocs" src={require("./img/jair.jpg")} />
+              <span className="text-footer">Jair Guetes Neto</span>
+            <img className="ajust-img-pocs" src={require("./img/mari.jpg")} />
+              <span className="text-footer">Marianne Linhares Monteiro</span>
+            <img className="ajust-img-pocs" src={require("./img/vini.jpg")} />
+              <span className="text-footer">Paulo Vinícios Soares</span>
+
           </div>
         </div>
       </div>
