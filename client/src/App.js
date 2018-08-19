91383609e48a42de502696b46e61b5d8ddecbfb9
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-08-18 21:04:46 -0300

    Adiciona espaço para italo inserir o gráfico da linha do tempo

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import $ from 'jquery'
+import $ from "jquery";
 import {
   XYPlot,
   XAxis,
@@ -17,60 +17,100 @@ import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import VotosVSInvestimentos from "./components/visualizacoes/VotosVSInvestimentos";
 
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="container-fluid">
           <div className="row part1">
-                <div className="col-md-6 col-xs-6 col-12 col-sm-12">
-                  <div className="left-container">
-                    <span>30%</span><h3>Lorem Ipsum dollor sit amet</h3>
-                    <img src={require("./img/img-left-part1.png")} />
-                  </div>
-                </div>
-                <div className="col-md-6 col-xs-6 col-12 col-sm-12 right-container">
-                  <img src={require("./img/logo.png")} />
-                  <h3>Pesquisa quantitativa não probabilística. Formulário respondido no Google Docs e compartilhado organicamente entre mulheres que trabalham em agência de publicidade</h3>
-                </div>
+            <div className="col-md-6 col-xs-6 col-12 col-sm-12">
+              <div className="left-container">
+                <span>30%</span>
+                <h3>Lorem Ipsum dollor sit amet</h3>
+                <img src={require("./img/img-left-part1.png")} />
+              </div>
+            </div>
+            <div className="col-md-6 col-xs-6 col-12 col-sm-12 right-container">
+              <img src={require("./img/logo.png")} />
+              <h3>
+                Pesquisa quantitativa não probabilística. Formulário respondido
+                no Google Docs e compartilhado organicamente entre mulheres que
+                trabalham em agência de publicidade
+              </h3>
+            </div>
           </div>
           <div className="row part2">
             <div className="col-md-12 col-xs-12 col-12 col-sm-12">
-                <GraficoBarras />
+              <GraficoBarras />
             </div>
             <div className="col-md-12 col-xs-12 col-12 col-sm-12 container-part2">
               <div className="row">
-                  <div className="col-md-6 col-xs-6 col-12 col-sm-12 text1-part2">
-                    <div>
-                      <span>30%</span><h3>Lorem Ipsum dollor sit amet</h3>
-                    </div>
+                <div className="col-md-6 col-xs-6 col-12 col-sm-12 text1-part2">
+                  <div>
+                    <span>30%</span>
+                    <h3>Lorem Ipsum dollor sit amet</h3>
                   </div>
-                  <div className="col-md-6 col-xs-6 col-12 col-sm-12 text2-part2">
-                      <h3>Pesquisa quantitativa não probabilística. Formulário respondido no Google Docs e compartilhado organicamente entre mulheres que trabalham em agência de publicidade</h3>
-                  </div>
+                </div>
+                <div className="col-md-6 col-xs-6 col-12 col-sm-12 text2-part2">
+                  <h3>
+                    Pesquisa quantitativa não probabilística. Formulário
+                    respondido no Google Docs e compartilhado organicamente
+                    entre mulheres que trabalham em agência de publicidade
+                  </h3>
+                </div>
               </div>
             </div>
           </div>
           <div className="row part3">
-            <h1 className="col-md-12 col-xs-12 col-12 col-sm-12">Lorem Ipsum dollor sit amet</h1>
+            <h1 className="col-md-12 col-xs-12 col-12 col-sm-12">
+              Lorem Ipsum dollor sit amet
+            </h1>
             <div className="col-md-8 col-xs-8 col-12 col-sm-12">
-                <VotosVSInvestimentos />
+              <VotosVSInvestimentos />
             </div>
             <div className="col-md-4 col-xs-4 col-12 col-sm-12 text-part3">
-                <div>
-                  <div className="row">
-                      <span>30%</span>
-                      <div className="ajuste-porcent-sub">
-                        <h3>Lorem Ipsum dollor sit amet</h3>
-                      </div>
+              <div>
+                <div className="row">
+                  <span>30%</span>
+                  <div className="ajuste-porcent-sub">
+                    <h3>Lorem Ipsum dollor sit amet</h3>
                   </div>
-                      <h3>Pesquisa quantitativa não probabilística. Formulário respondido no Google Docs e compartilhado organicamente entre mulheres que trabalham em agência de publicidade</h3>
                 </div>
+                <h3>
+                  Pesquisa quantitativa não probabilística. Formulário
+                  respondido no Google Docs e compartilhado organicamente entre
+                  mulheres que trabalham em agência de publicidade
+                </h3>
+              </div>
+            </div>
+          </div>
+          <div className="row partItalo">
+            <h1 className="col-md-12 col-xs-12 col-12 col-sm-12">
+              Lorem Ipsum dollor sit amet
+            </h1>
+            <div className="col-md-8 col-xs-8 col-12 col-sm-12">
+              {/*ITALO INSIRA SEU GRAFICO AQUI*/}
+            </div>
+            <div className="col-md-4 col-xs-4 col-12 col-sm-12 text-part3">
+              <div>
+                <div className="row">
+                  <span>30%</span>
+                  <div className="ajuste-porcent-sub">
+                    <h3>Lorem Ipsum dollor sit amet</h3>
+                  </div>
+                </div>
+                <h3>
+                  Pesquisa quantitativa não probabilística. Formulário
+                  respondido no Google Docs e compartilhado organicamente entre
+                  mulheres que trabalham em agência de publicidade
+                </h3>
+              </div>
             </div>
           </div>
           <div className="row part4">
-            <h1 className="col-md-12 col-xs-12 col-12 col-sm-12">Lorem Ipsum dollor sit amet</h1>
+            <h1 className="col-md-12 col-xs-12 col-12 col-sm-12">
+              Lorem Ipsum dollor sit amet
+            </h1>
             <div className="col-md-12 col-xs-12 col-12 col-sm-12">
               <div className="row">
                 <div className="col-md-4 col-xs-4 col-12 col-sm-12">
@@ -87,7 +127,9 @@ class App extends Component {
                       <div className="ghost-size">
                         <img src={require("./img/pac-man-ghost.svg")} />
                       </div>
-                      <div className="nota"><h3>321 fantasmas</h3></div>
+                      <div className="nota">
+                        <h3>321 fantasmas</h3>
+                      </div>
                     </div>
                     <div className="btn">
                       <div className="denuncia">
@@ -97,7 +139,7 @@ class App extends Component {
                   </div>
                 </div>
 
-                 <div className="col-md-4 col-xs-4 col-12 col-sm-12">
+                <div className="col-md-4 col-xs-4 col-12 col-sm-12">
                   <div className="card">
                     <div className="row rr-1">
                       <div className="rr1-img-part">
@@ -111,7 +153,9 @@ class App extends Component {
                       <div className="ghost-size">
                         <img src={require("./img/pac-man-ghost.svg")} />
                       </div>
-                      <div className="nota"><h3>321 fantasmas</h3></div>
+                      <div className="nota">
+                        <h3>321 fantasmas</h3>
+                      </div>
                     </div>
                     <div className="btn">
                       <div className="denuncia">
@@ -121,7 +165,7 @@ class App extends Component {
                   </div>
                 </div>
 
-            <div className="col-md-4 col-xs-4 col-12 col-sm-12">
+                <div className="col-md-4 col-xs-4 col-12 col-sm-12">
                   <div className="card">
                     <div className="row rr-1">
                       <div className="rr1-img-part">
@@ -135,7 +179,9 @@ class App extends Component {
                       <div className="ghost-size">
                         <img src={require("./img/pac-man-ghost.svg")} />
                       </div>
-                      <div className="nota"><h3>321 fantasmas</h3></div>
+                      <div className="nota">
+                        <h3>321 fantasmas</h3>
+                      </div>
                     </div>
                     <div className="btn">
                       <div className="denuncia">
@@ -144,7 +190,6 @@ class App extends Component {
                     </div>
                   </div>
                 </div>
-
               </div>
             </div>
           </div>
@@ -153,6 +198,5 @@ class App extends Component {
     );
   }
 }
-
 
 export default App;
